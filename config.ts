7c2ac9edfbb3a6129344c9e966cf8affd8f975b2
Bar Gabay <bar.gabay.home@gmail.com>
2022-09-21 18:28:18 +0300

    feat: backend added

--- config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config();
-
-export const config = {
-  SERVER_PORT: Number(process.env.SERVER_PORT) || 3000
-}
-
-export const uri = "mongodb://127.0.0.1:27017";
-
