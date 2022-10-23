a25972d7454eed83147fe870bfeffcfdfaf34251
Bar Gabay <bar.gabay.home@gmail.com>
2022-10-23 11:49:59 +0300

    docker files and compose template

--- backend/src/person/router.ts
+++ backend/src/person/router.ts
@@ -5,13 +5,13 @@ const personRoute: Router = express.Router();
   
 personRoute.get("/person/All/The/People", getAllPeopleC);
 personRoute.get("/person/:id", getPersonByIDC); //
-personRoute.get("/person/:name/:id", getPersonInGroupByNameC); //
-personRoute.get("/person/All/groups/:id", getAllGroupsOfPersonC); // 
-personRoute.get("/person/populated", getPopulatedPeople); //
+personRoute.get("/person/:name/:id", getPersonInGroupByNameC); // ..
+personRoute.get("/person/All/groups/:id", getAllGroupsOfPersonC); // ..
+personRoute.get("/person/populated", getPopulatedPeople); // ..
 
-personRoute.delete("/person/:id", deletePersonByIDC); //
+personRoute.delete("/person/:id", deletePersonByIDC); // ..
 
-personRoute.post("/person", createPersonC); // 
-personRoute.post("/person/update/:id", updatePersonByIDC); //
+personRoute.post("/person", createPersonC); // ..
+personRoute.post("/person/update/:id", updatePersonByIDC); // ..
 
 export default personRoute;
