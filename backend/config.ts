85f20863ff035dd39f512a4a8f8314d7d10901fe
Bar Gabay <bar.gabay.home@gmail.com>
2022-11-08 11:37:40 +0200

    not to check

--- backend/config.ts
+++ backend/config.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {
+const config = {
   SERVER_PORT: Number(process.env.SERVER_PORT) || 3001,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
 };
+
+export default config;
